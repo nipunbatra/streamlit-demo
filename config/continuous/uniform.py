214a02fae0199a91881586a5aae6d71df46834f9
deepuha06 <deepuha0604@gmail.com>
2025-03-18 18:03:04 +0530

    change uniform distribution

--- config/continuous/uniform.py
+++ config/continuous/uniform.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import torch
 
+def get_uniform_params():
+    b = st.sidebar.slider("Upper Bound (b)", -10.0, 10.0, 5.0)
+    a_min = -10.0  
+    a = st.sidebar.slider("Lower Bound (a)", a_min, b, max(a_min, b-0.01))
+    return {"a": a, "b": b}
+
 UNIFORM = {
-    "params": lambda: {
-        "a": st.sidebar.slider("Lower Bound (a)", -10.0, 10.0, -5.0),
-        "b": st.sidebar.slider("Upper Bound (b)", -10.0, 10.0, 5.0),
-    },
+    "params": get_uniform_params,
     "dist": lambda p: torch.distributions.Uniform(p["a"], p["b"]),
     "support": lambda p: (p["a"], p["b"]),
 }
